f0453fb60690cb3b4b8684718332a27e7d43ca45
NicolasContrerasMoreira <ncontrerasmoreira@itba.edu.ar>
2022-06-20 16:45:41 -0300

    main

--- Javascript.js
+++ Javascript.js
@@ -3,40 +3,36 @@ console.log("Hola");
 function sumarprecio(valorproducto) {
   Total = Total + valorproducto;
   document.getElementById("Total").innerHTML =
-    "TOTAL: " + "$" + Total + " Pesos";
-}
+    "TOTAL: " + "$" + Total + " Pesos";}
+
 function Borrar() {
   Total = 0;
-  document.getElementById("Total").innerHTML = "TOTAL:"  + " $" + 0 + " Pesos";
-}
+  document.getElementById("Total").innerHTML = "TOTAL:"  + " $" + 0 + " Pesos";}
 
 let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+  showSlides((slideIndex += n));}
 
 function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+  showSlides((slideIndex = n));}
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
-    slideIndex = 1;
-  }
+    slideIndex = 1;}
+    
   if (n < 1) {
-    slideIndex = slides.length;
-  }
+    slideIndex = slides.length;}
+
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+    slides[i].style.display = "none";}
+
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+    dots[i].className = dots[i].className.replace(" active", "");}
+
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
+  dots[slideIndex - 1].className += " active";}
