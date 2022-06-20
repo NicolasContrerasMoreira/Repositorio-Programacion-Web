820711ceabff30a1a47cfcee36e2fa5b40d5d8c3
NicolasContrerasMoreira <ncontrerasmoreira@itba.edu.ar>
2022-06-20 17:16:06 -0300

    main

--- Javascript.js
+++ Javascript.js
@@ -3,20 +3,24 @@ console.log("Hola");
 function sumarprecio(valorproducto) {
   Total = Total + valorproducto;
   document.getElementById("Total").innerHTML =
-    "TOTAL: " + "$" + Total + " Pesos";}
+    "TOTAL: " + "$" + Total + " Pesos";
+}
 
 function Borrar() {
   Total = 0;
-  document.getElementById("Total").innerHTML = "TOTAL:"  + " $" + 0 + " Pesos";}
+  document.getElementById("Total").innerHTML = "TOTAL:"  + " $" + 0 + " Pesos";
+}
 
 let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides((slideIndex += n));}
+  showSlides((slideIndex += n));
+}
 
 function currentSlide(n) {
-  showSlides((slideIndex = n));}
+  showSlides((slideIndex = n));
+}
 
 function showSlides(n) {
   let i;
@@ -24,7 +28,7 @@ function showSlides(n) {
   let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;}
-    
+
   if (n < 1) {
     slideIndex = slides.length;}
 
@@ -35,4 +39,5 @@ function showSlides(n) {
     dots[i].className = dots[i].className.replace(" active", "");}
 
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";}
+  dots[slideIndex - 1].className += " active";
+}
